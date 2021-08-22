a8b9f392b084bbdabe8edc18bd45fa83b666198d
YuxuanLuan <luanyx1126@gmail.com>
2021-08-22 00:18:33 -0400

    Wrapped the seasonDisplay webpage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  //more js function, ini the state
+  //create new instance, will be automatically and instantly called
+  // jump to render as soon as state method, before the geolocation runs
+  state = { lat: null, errorMsg: "" };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  //componentDidMount  -> only get invoke one time -> data loading
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMsg: err.message })
+    );
+  }
+  //componentDidUpdate -> get called every single time  -> state/props change
+  //componentWillUnmount -> do the cleanup (non-react stuff)
+
+  renderContent() {
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error: {this.state.errorMsg}</div>;
+    }
+
+    if (!this.state.errorMsg && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return (
+      <div>
+        <Spinner message="Please accept location request" />
+      </div>
+    );
+  }
+
+  //have to define render
+  //be called many times
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
